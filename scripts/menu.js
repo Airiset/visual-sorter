03026c91af1fb26c6eec939062c86b70b10c6b3d
Airiset <eric2000pima@gmail.com>
2021-03-11 00:21:04 -0500

    Add sort algorithm selection functionality
    This commit adds the ability to access the currently selected
    algorithm from the selection dialog box object. It also fixes the
    addAlgorithmOptions function that adds all algorithms names as options
    to the selection dialog.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,10 @@
 let selectionDialog = document.getElementById("algorithm-selector");
 
+/**
+ * Adds algorithm name as options on the given selection dialog.
+ * @param selectionDialog  the HTML DOM object representing the
+ *                         selection dialog
+ */
 function addAlgorithmOptions(selectionDialog) {
     let sortNames = getAlgorithmNames();
 
@@ -8,11 +13,25 @@ function addAlgorithmOptions(selectionDialog) {
         let optionObject = document.createElement("OPTION");
         let sortName = sortNames[i];
 
-        optionObject.name = sortName;
+        // set default sorting algorithm to be the first in the list
+        if (i === 0) {
+            optionObject.selected = true;
+        }
+
+        optionObject.label = sortName;
         optionObject.value = sortName;
 
         selectionDialog.add(optionObject);
     }
+}
+
+/**
+ * Return the name ID of the currently selected algorithm.
+ * @returns {string}
+ */
+function getSelectedAlgorithmName() {
+    let index = selectionDialog.selectedIndex;
+    return selectionDialog.options[index].label;
 }
 
 addAlgorithmOptions(selectionDialog);
